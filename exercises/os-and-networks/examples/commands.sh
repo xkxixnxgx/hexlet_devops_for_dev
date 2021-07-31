75b43d302907c377564c6d15a32bd0eb40659af5
@hexlet/cli <support@hexlet.io>
2021-07-31 19:26:05 +0300

    @hexlet/cli: submit

---
+++ exercises/os-and-networks/examples/commands.sh
@@ -0,0 +1,8 @@
+vagrant init <box_name> # Инициализация
+vagrant up # Запуск виртуальной машины
+vagrant ssh # Подключение к машине
+vagrant halt # Остановка машины
+vagrant reload # Перезапуск машины halt & up
+
+# Удаляет директорию .vagrant (техническую директорию для созданной виртуальной машины)
+vagrant destroy
